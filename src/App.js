d5145e64f46af57ba70dbe7036e17ee606ee4955
Mikiyas T <mikiyasegaye@gmail.com>
2021-09-26 23:10:28 +0300

    feat: add createBrowserHistory to the Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import { Routes } from "./routes";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router history={createBrowserHistory}>
         <Routes />
       </Router>
     </div>
