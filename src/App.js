75b1cf14174459763868ec0aca7f631853d64d5a
Mikiyas T <mikiyasegaye@gmail.com>
2021-09-26 23:26:54 +0300

    fix: get all back to as it was

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import { Routes } from "./routes";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router history={createBrowserHistory}>
+      <Router>
         <Routes />
       </Router>
     </div>
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,13 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import SideBar from "./components/SideBar";
-import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export const Routes = () => {
-  const { pathname } = useLocation();
   return (
     <div>
       <SideBar />
       <Switch>
-        <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
           <Redirect to="/Home" />
